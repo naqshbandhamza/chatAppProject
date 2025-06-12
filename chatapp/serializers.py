c67a334f0e8c15107398556c1078fc1f5c799b81
hamza <naqshband.hamza@gmail.com>
2025-06-12 15:39:10 +0500

    changes

--- chatapp/serializers.py
+++ chatapp/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.hashers import make_password
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
